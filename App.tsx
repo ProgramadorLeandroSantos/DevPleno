8c2f4d85c9f5dae3258bfc973e492582699cb3be
ProgramadorLeandroSantos <programadorleandrosantos@gmail.com>
2022-12-27 01:01:04 -0300

    feature: settings pushNotifications

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -9,10 +9,53 @@ import {
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import codePush from "react-native-code-push";
+import { firebase } from '@react-native-firebase/messaging';
 
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+
+  const loadStorageData = async () => {
+
+    const authStatus = await firebase.messaging().requestPermission();
+
+    const enabled =
+      authStatus === firebase.messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === firebase.messaging.AuthorizationStatus.PROVISIONAL;
+
+    console.log('CAN I ACCESS FIREBASE: ', enabled);
+
+    if (enabled) {
+      const token = await firebase.messaging().getToken();
+
+      console.log('DEVICE_TOKEN: ', token);
+
+      firebase.messaging().onMessage(async (remoteMessage) => {
+        console.log({ log: 'RECEBENDO O PUSH COM O APP ABERTO', remoteMessage });
+      });
+
+      firebase.messaging().onNotificationOpenedApp(async (remoteMessage) => {
+        console.log({ log: 'ENTROU NO onNotificationOpenedApp', remoteMessage });
+      });
+
+      firebase
+        .messaging()
+        .getInitialNotification()
+        .then(async (remoteMessage) => {
+          if (remoteMessage) {
+            console.log({ log: 'RECEBENDO O PUSH COM O APP FECHADO/SEGUNDO PLANO', remoteMessage });
+          }
+        });
+
+      firebase.messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+        console.log({ log: 'ENTROU NO setBackgroundMessageHandler', remoteMessage });
+      });
+    }
+  };
+
+  useEffect(() => {
+    loadStorageData();
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
